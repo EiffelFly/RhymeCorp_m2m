64214f4c3d39f1d46b4c1c0cb608491ca3acde10
eiffelfly <eric525282@gmail.com>
2020-07-08 14:04:58 +0800

    Major: remove api

--- hololink/hololink/urls.py
+++ hololink/hololink/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
 from .views import index
-from rest_framework import routers
-from api import views
-
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
@@ -13,14 +10,3 @@ urlpatterns = [
     path('', index, name='index'),
 ]
 
-
-router = routers.DefaultRouter()
-router.register('users', views.UserViewSet)
-router.register('groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns += [
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
